b6fa90f36d79731b5788fd237a0721e3f14c62c4
garrettkay <garrettkay@gmail.com>
2024-08-28 01:28:17 -0700

    problem 38, notice for a pandigital multiple to be greater than that of 9, (1,2,3,4,5), we would need a 4 digit number and (1,2), (first number must start with 9, it is clear that such a pandigital multiple is not possible with fewer than 4 digits), check each 4 digit number > 9000

---
+++ pandigitalmultiples.py
@@ -0,0 +1,10 @@
+import itertools
+
+perms = itertools.permutations([9,8,7,6,5,4,3,2,1])
+pandigitalmultiples = []
+
+for i in range(1000,10000):
+	if set(map(int,str(i))).union(set(map(int,str(i * 2)))) == set([1,2,3,4,5,6,7,8,9]):
+		pandigitalmultiples.append(i * 100000 + i * 2)
+
+print(pandigitalmultiples)
