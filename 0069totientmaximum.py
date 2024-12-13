3d973a9131346ef1dfcc82a8ec490e88124ebabe
garrettkay <garrettkay@gmail.com>
2024-12-12 21:59:34 -0800

    problem 69, notice euler totient function is multiplicative, indicating that n/phi(n) is also multiplicative. we can also see that n/phi(n) does not change when we multiply n by prime factors of n. thus, n/phi(n) can be completely defined by phi(p_i), where p_i are the prime factors of n. using this, along with the fact that phi(p) = p-1 for p prime, we can populate a dictionary for all values from 2 to 1 million. we also notice that for primes p, phi(p) is greatest when p is smallest. thus, we could have equivalently computed the max n/phi(n) by multipling the smallest primes until the next iteration would result in a number greater than 1 million

---
+++ 0069totientmaximum.py
@@ -0,0 +1,39 @@
+import math
+import sympy
+
+primefactors = dict()
+
+def primefactorize(n):
+	try:
+		return primefactors[n]
+	except:
+		if sympy.isprime(n):
+			primefactors.update({n:{n}})
+			return {n}
+		for i in range(2,math.isqrt(n) + 1):
+			if n % i == 0:
+				factors1 = primefactorize(i)
+				factors2 = primefactorize(n//i)
+				primefactors.update({n:factors1.union(factors2)})
+				return factors1.union(factors2)
+
+nphiratio = dict()
+maxnphiratio = 0
+maxn = 0
+for i in range(2,1000001):
+	factors = primefactorize(i)
+	if len(factors) == 1:
+		nphiratio[i] = i / (i - 1)
+		if i / (i - 1) > maxnphiratio:
+			maxnphiratio = i / (i - 1)
+			maxn = i
+	else:
+		product = 1
+		for factor in factors:
+			product *= nphiratio[factor]
+		nphiratio[i] = product
+		if product > maxnphiratio:
+			maxnphiratio = product
+			maxn = i
+
+print("n =",maxn,"n / phi =",maxnphiratio)
