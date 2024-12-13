75b50f31e9b95d990cdbe4b8dfcc3acaecf0d7f5
garrettkay <garrettkay@gmail.com>
2024-12-12 23:21:44 -0800

    problem 72, notice that the number of new fractions created for each denominator is equal to its euler totient, simply add all phi(n) for n in [2,1000000]

--- 0072countingfractions.py
+++ 0072countingfractions.py
@@ -1,7 +1,7 @@
 import math
 import sympy
 
-"""primefactors = dict()
+primefactors = dict()
 
 def primefactorize(n):
 	try:
@@ -18,9 +18,8 @@ def primefactorize(n):
 				return factors1 + factors2
 
 phi = dict()
-nphiratiomin = 100
-nmin = 0
-for i in range(2,10**7):
+
+for i in range(2,1000001):
 	factors = primefactorize(i)
 	seenfactors = set()
 	product = 1
@@ -35,24 +34,5 @@ for i in range(2,10**7):
 				seenfactors.add(factor)
 				product *= phi[factor]
 		phi[i] = product
-		if sorted(map(int,str(i))) == sorted(map(int,str(product))):
-			if i/phi[i] < nphiratiomin:
-				nphiratiomin = i/phi[i]
-				nmin = i
-print(nmin,phi[nmin],nphiratiomin)"""
 
-primes = list(sympy.primerange(5000))
-
-phimin = 100
-nmin = 0
-for p in range(len(primes)):
-	for q in range(p,len(primes)):
-		phi = (primes[p] - 1) * (primes[q] - 1)
-		n = primes[p] * primes[q]
-		if n > 10**7:
-			break
-		if sorted(map(int,str(n))) == sorted(map(int,str(phi))):
-			if n/phi < phimin:
-				phimin = n/phi
-				nmin = n
-print(nmin,phimin,nmin/phimin)
+print(sum(phi.values()))
