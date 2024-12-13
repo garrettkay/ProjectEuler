87ef9ea97e98cc6e65fa4250810be725a5cca9ae
garrettkay <garrettkay@gmail.com>
2024-12-12 20:47:30 -0800

    problem 66, there is a well known algorithm to find the fundamental solutions to pell's equation (name of this diophantine equation) that utilizes the continued fraction expansion of sqrt(D). use the solution from problem 64 to find the periodic continued fractions of square roots and the solution from problem 57 to generate approximations of those square roots, this gives the fundamental solutions to pell's equation

---
+++ 0066diophantineequation.py
@@ -0,0 +1,38 @@
+import math
+
+def rootperiod(n):
+	frac = [str(math.isqrt(n)) + ";"]
+	num = 1
+	rem = -math.isqrt(n)
+	if math.sqrt(n).is_integer():
+		return (0,frac)
+	while True:
+		nextfracterm = int(num * (math.sqrt(n) - rem) / (n - rem ** 2))
+		frac.append(nextfracterm)
+		num, rem = int((n - rem ** 2)/num), int((num * -rem - nextfracterm * (n - rem ** 2)) / num)
+		if num == 1 and rem == -math.isqrt(n):
+			break
+	return (len(frac) - 1,frac)
+
+maxX = 0
+maxY = 0
+maxD = 0
+for D in range(1,1001):
+	contfrac = rootperiod(D)
+	if contfrac[0] % 2 == 0:
+		xyapprox = ([int(contfrac[1][0][:-1])] + contfrac[1][1:])[:-1]
+	else:
+		xyapprox = ([int(contfrac[1][0][:-1])] + 2 * contfrac[1][1:])[:-1]
+	num = 1
+	den = 0
+	for value in xyapprox.__reversed__():
+		num,den = value*num + den,num
+	x = num
+	y = den
+	# print(y,"** 2 *",D,"+ 1 =",x,"** 2")
+	if x > maxX:
+		maxX = x
+		maxY = y
+		maxD = D
+print("Our max value of x satisfies the equation:",maxY,"** 2 *",maxD,"+ 1 =",maxX,"** 2")
+print("x:",maxX,"y:",maxY,"D:",maxD)
