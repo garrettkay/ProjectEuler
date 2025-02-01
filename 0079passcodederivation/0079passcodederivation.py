04e1d40930b56835599b42214fadbc9b1371a9cd
garrettkay <garrettkay@gmail.com>
2025-02-01 15:32:21 -0800

    problem 79, since we know for each correct subcode, the first character must occur before the second, which must occur before the third in the original passcode, we can decompose the subcodes into a series of directed edges. then, if we have a directed edge (a,c), we can delete it if we also have directed edges (a,b) and (b,c) since the phrase abc would satisfy all 3 orderings with only two edges. the same is possible for edge (a,d) if there exists (a,b), (b,c), and (c,d), and so on. we only needed chains of length 2 and 3 to solve this problem

---
+++ 0079passcodederivation/0079passcodederivation.py
@@ -0,0 +1,31 @@
+edges = set()
+
+with open("0079passcodederivation/0079_keylog.txt", 'r') as file:
+	for line in file:
+		edges.add((int(line[0]),int(line[1])))
+		edges.add((int(line[1]),int(line[2])))
+
+def deletel1edge():
+	for edge in edges:
+		for node in range(10):
+			if (edge[0],node) in edges and (node,edge[1]) in edges:
+				edges.remove(edge)
+				return True
+	return False
+
+while deletel1edge():
+	pass
+
+def deletel2edge():
+	for edge in edges:
+		for node1 in range(10):
+			for node2 in range(10):
+				if (edge[0],node1) in edges and (node1,node2) in edges and (node2,edge[1]) in edges:
+					edges.remove(edge)
+					return True
+	return False
+
+while deletel2edge():
+	pass
+
+print(edges)
