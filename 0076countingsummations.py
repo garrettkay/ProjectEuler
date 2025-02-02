e801f6452beee6c32259cd0872aa50776fb37c71
garrettkay <garrettkay@gmail.com>
2025-02-01 16:30:30 -0800

    update to problem 76 using a more efficient solution found by attempting to solve problem 78.

--- 0076countingsummations.py
+++ 0076countingsummations.py
@@ -1,28 +1,22 @@
-summations = dict({})
+summations = dict()
 
-for n in range(1,101):
-	for x in range(1,101):
-		summations[(n,1,x)] = max(n//2 - x + 1,0)
-
-def numofdivisionsgreaterthanx(n,d,x):
+def numofdivisionsgreaterthanx(n,x):
+	if n == 0:
+		return 1
+	if n < x:
+		return 0
+	if n == x:
+		summations[(n,x)] = 1
+		return 1
 	try:
-		return summations[(n,d,x)]
+		return summations[(n,x)]
 	except:
 		sum = 0
-		for i in range(x,n//d + 1):
-			sum += numofdivisionsgreaterthanx(n - i, d - 1, i)
-		if sum == 0:
-			for j in range(x,n + 1):
-				summations[(n,d,j)] = 0
-		else:
-			summations[(n,d,x)] = sum
+		for i in range(n,0,-1):
+			sum += numofdivisionsgreaterthanx(n - i, i)
+			summations[(n,i)] = sum
 		return sum
 
-for n in range(2,101):
-	for d in range(1,n):
-		numofdivisionsgreaterthanx(n,d,1)
-
-count = 0
-for i in range(1,100):
-	count += summations[(100,i,1)]
-print(count)
+for n in range(1,101):
+	numdiv = numofdivisionsgreaterthanx(n,1)
+print(summations[(100,1)] - 1)
