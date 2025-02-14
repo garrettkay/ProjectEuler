e7116c55fc94d64b231241e3c2209968e8f81290
garrettkay <garrettkay@gmail.com>
2025-02-13 17:30:12 -0800

    problem 86, notice the shortest path across the surface of a cuboid can be found by creating a net of the cuboid. we can see a straight line path that takes the longest dimension in one direction, and the sum of the two shorter dimensions in the other direction. thus, this problem becomes a question of pythagorean triples that fit the criterion for being an appropriate cuboid where the longest dimension is one of the legs of the triple. we can quickly calculate the number of such cuboids and classify them by M = longest dimension. then, sum from M = 1 until we reach 1 million cuboids.

---
+++ 0086cuboidroute.py
@@ -0,0 +1,24 @@
+import collections
+import math
+
+integerpaths = collections.defaultdict(int)
+
+for m in range(2,500):
+	for n in range(1,m):
+		if (m - n) % 2 == 0 or math.gcd(m,n) != 1:
+			continue
+		a,b = m ** 2 - n ** 2, 2 * m * n
+		for i in range(1,5000):
+			if min(a,b) * i > 5000:
+				break
+			integerpaths[max(a,b) * i] += (min(a,b) * i) // 2
+			integerpaths[min(a,b) * i] += max(0,(min(a,b) * i) + 1 + (-i * max(a,b)) // 2)
+
+sum = 0
+for i in range(1000000):
+	sum += integerpaths[i]
+	if i == 99 or i == 100:
+		print(i,sum)
+	if sum > 1000000:
+		print(i,sum)
+		break
