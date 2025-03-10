4e0e73f0266b3a58ac29cf4bdacd3298dd735b4a
garrettkay <garrettkay@gmail.com>
2025-03-09 22:16:26 -0700

    first, notice that the only subset pairs you need to check are ones that are the same size, which immediately follows from the second rule being satisfied. then, realize that we only need to check subsets between size 2 and half of the total size of the set (set being strictly increasing means no elements have the same value (size 1) and a subset with over half the total set cannot be disjoint from another subset of the same size). now, notice for any combination of n * 2 elements from the set, there is a consistent number of divisions (dependent on n) that yield two equal size subsets that need to be checked for equality (this follows from distinct elements). we can use these two facts to generate all n * 2 element combinations of the set, multiply by the number of divisions that require a check for equality, and sum over all subset sizes n for a given set. from inspection, the number of divisions seems to follow the pattern (n * 2 - 1) C (n * 2 - 4) /2, but the reason for this is not yet obvious.

---
+++ 0106specialsubsetsumsmetatesting.py
@@ -0,0 +1,34 @@
+import math
+import itertools
+
+def subsettests(n):
+	count = 0
+	for i in range(2,n//2 + 1):
+		count += math.comb(n,i * 2) * splitsubsets(i * 2)
+	return count
+
+def splitsubsets(n):
+	count = 0
+	for comb in itertools.combinations(range(1,n+1),n // 2):
+		set1 = set(comb)
+		set2 = set(range(1,n+1)) - set1
+		set1larger = False
+		set2larger = False
+		while set1:
+			if max(set1) > max(set2):
+				if set2larger:
+					count += 1
+					break
+				else:
+					set1larger = True
+			else:
+				if set1larger:
+					count += 1
+					break
+				else:
+					set2larger = True
+			set1.remove(max(set1))
+			set2.remove(max(set2))
+	return count // 2
+
+print(subsettests(12))
